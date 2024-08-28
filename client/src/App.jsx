92bf203b6bfd6b2417b9f41021106ab40d08268e
Nikhilsingh9211 <rajatsweet.1234@gmail.com>
2024-08-28 11:33:23 +0530

    Components added and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Projects from "./components/Projects";
+import Dashoard from "./components/Dashoard";
+
 function App() {
   return (
     <>
       <p className="text-3xl text-red-500">Hello World</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/dashoard" element={<Dashoard />} />
+        </Routes>
+      </Router>
     </>
   );
 }
